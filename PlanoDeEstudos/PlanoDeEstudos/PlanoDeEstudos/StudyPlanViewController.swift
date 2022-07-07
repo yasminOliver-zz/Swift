298199cd6a6a3c7d2a81a44ddd4543e4d0d14dae
Yasmin Oliveira <81441089+yasminOliver@users.noreply.github.com>
2022-07-07 16:29:00 -0300

    App finished

---
+++ PlanoDeEstudos/PlanoDeEstudos/PlanoDeEstudos/StudyPlanViewController.swift
@@ -0,0 +1,51 @@
+//
+//  StudyViewController.swift
+//  PlanoDeEstudos
+//
+//  Created by Eric Brito
+//  Copyright © 2017 Eric Brito. All rights reserved.
+
+import UIKit
+import UserNotifications
+
+class StudyPlanViewController: UIViewController {
+
+    @IBOutlet weak var tfCourse: UITextField!
+    @IBOutlet weak var tfSection: UITextField!
+    @IBOutlet weak var dpDate: UIDatePicker!
+    
+    let sm = StudyManager.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dpDate.minimumDate = Date()
+        
+
+    }
+
+    @IBAction func schedule(_ sender: UIButton) {
+        let id = String(Date().timeIntervalSince1970)
+        let studyPlan = StudyPlan(course: tfCourse.text!, section: tfSection.text!, date: dpDate.date, done: false, id: id)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Lembrete"
+        content.subtitle = "Matéria: \(studyPlan.course)"
+        content.body = "Estudar \(studyPlan.section)"
+        
+//        content.sound = UNNotificationSound(named: "Shopee-Notification.caf")
+        
+        
+        content.categoryIdentifier = "Lembrete"
+        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 15, repeats: false)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: dpDate.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+        
+        sm.addPlan(studyPlan)
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
